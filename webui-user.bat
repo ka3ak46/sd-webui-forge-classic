b46f1913bf887a22ff4d4311858b1940b492757b
ka3ak46 <ka3ak46@gmail.com>
2026-04-28 11:11:54 +0300

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,13 +1,13 @@
 @echo off
+git pull
 
 :: set PYTHON=
 :: set GIT=
 :: set VENV_DIR=
 
-set COMMANDLINE_ARGS= --uv
-
-:: --xformers --sage --uv
-:: --pin-shared-memory --cuda-malloc --cuda-stream
+set COMMANDLINE_ARGS= --uv --nunchaku --bnb --cuda-malloc --onnxruntime-gpu --reserve-vram 2
+:: --xformers --enable-insecure-extension-access --sage --uv
+:: --pin-shared-memory --cuda-malloc --cuda-stream 
 :: --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install
 
 call webui.bat
